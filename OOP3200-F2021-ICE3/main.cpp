f6257c28c0d6f869be5686745c4a0f447088e9d3
Katherine Bellman <katherine.bellman@dcmail.ca>
2021-09-29 20:50:35 -0400

    Add project files.

---
+++ OOP3200-F2021-ICE3/main.cpp
@@ -0,0 +1,16 @@
+// main.cpp
+//
+//  @Author: Katherine Bellman
+//  @Student number: 100325825
+//
+//  @Date: September 29th 2021
+//  @Project description: ICE 3 - Inheritance
+//
+
+#include <iostream>
+
+int main()
+{
+    
+}
+
